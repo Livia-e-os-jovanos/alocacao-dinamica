198c93938fb3ab2fbb5622d7f623a1e5451cd7a3
Geovanni Martins <geovanni.mrts@gmail.com>
2025-09-22 21:09:07 -0300

    realização da questão 3

---
+++ questao3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Crie uma matriz m × n usando ponteiro de ponteiro (int **).
+Preencha cada posição com i + j e exiba a matriz.
+*/
+int main () {
+
+    //tamanho da matriz
+    int m, n;
+    printf("Digite o número de linhas: ");
+    scanf("%d", &m);
+    printf("Digite o número de colunas: ");
+    scanf("%d", &n);
+
+    // aloca para um vetor de ponteiros
+    int **p = malloc(m * sizeof(p));
+    if (p == NULL) {
+        printf("Falha ao alocar memória em p\n");
+        return 1;
+    }
+
+    // cada item vai ter espaço para um vetor
+    for (int i = 0; i < m; i++) {
+        p[i] = malloc(n * sizeof(p));
+        if (p[i] == NULL) {
+            printf("Falha ao alocar memória para a linha p[%d]\n", i);
+            return 1;
+        }
+    }
+
+    // usuário preenche vetor
+    for (int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &p[i][j]);
+        }
+    }
+
+    //exibe
+    for (int i = 0; i < m; i++) {
+        printf("\n");
+        for(int j = 0; j < n; j++) {
+            printf("%d ", p[i][j]);
+        }
+    }
+
+    // libera
+    for (int i = 0; i < m; i++) {
+        free(p[i]);
+    }
+    free(p);
+}
